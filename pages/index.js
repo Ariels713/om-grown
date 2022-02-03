4b467738c0d0c0d5fe670407498b7b1f81cf3c7c
Ariels713 <Ariels713@gmail.com>
2022-02-03 10:01:16 -0500

    working on layaout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
-import MainHero from "../components/mainHero/MainHero"
-import MainContent from "../components/mainContent/MainContent"
-import NewestItems from "../components/newestItems/NewestItems"
+import Head from 'next/head'
+import MainHero from '../components/mainHero/MainHero'
+import MainContent from '../components/mainContent/MainContent'
+import NewestItems from '../components/newestItems/NewestItems'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>OM-GROWN</title>
+        <meta name='description' content='Css Documentation and examples' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <MainHero />
-      <MainContent />
       <NewestItems />
+      <MainContent />
     </>
   )
 }

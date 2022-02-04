4ab0bce9697809f469bd815a5912fc280b27c676
Ariel Rodriguez <arielrodriguez@Ariels-Mac-mini.local>
2022-02-03 22:48:20 -0500

    updated settings for shopify

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SHOPIFY_STOREFONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFONT_ACCESSTOKEN,
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+  },
 }
